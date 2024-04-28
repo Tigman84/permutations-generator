3892147d402d4599db4d845eb40fe1db25d347f5
Tigman84 <153453242+Tigman84@users.noreply.github.com>
2024-04-28 23:57:17 +0400

    Create permutations.cpp

---
+++ permutations.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void generatePermutations(vector<T>& nums, int start, vector<vector<T>>& result) {
+    if (start == nums.size() - 1) {
+        result.push_back(nums); 
+        return;
+    }
+    for (int i = start; i < nums.size(); ++i) {
+        swap(nums[start], nums[i]);
+        generatePermutations(nums, start + 1, result);
+        swap(nums[start], nums[i]);
+    }
+}
+
+template<typename T>
+void printPermutations(const vector<vector<T>>& permutations) {
+    for (const auto& perm : permutations) {
+        for (const T& element : perm) {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> permutations;
+    generatePermutations(nums, 0, permutations);
+    cout << "Permutations:" << endl;
+    printPermutations(permutations);
+    cout << "Total number of permutations: " << factorial(nums.size()) << endl;
+    return 0;
+}
